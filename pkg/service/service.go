885810872547cfba51d952558f1afecd6342e2da
fr13n8 <ovsepanrik@gmail.com>
2021-05-27 12:45:22 +0300

    Add dockerfile, docker-compose and write unit-test for middleware and auth

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -5,6 +5,8 @@ import (
 	"github.com/fr13n8/todo-app/pkg/repository"
 )
 
+//go:generate mockgen -source=service.go -destination=mocks/mock.go
+
 type Authorization interface {
 	CreateUser(user todo.User) (int, error)
 	GenerateToken(username, password string) (string, error)
